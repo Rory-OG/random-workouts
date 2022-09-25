ac54555b60dd58934ffb05dbcfc01af873b7177f
Rory O'Gallagher <rory.j.ogallagher@gmail.com>
2022-09-25 15:09:16 -0400

    adding features towards MVP

--- new_app/app.R
+++ new_app/app.R
@@ -1,22 +1,49 @@
 library(gridlayout)
+library(import)
 library(shiny)
 library(ggplot2)
 library(googlesheets4)
+library(dplyr)
 
 # Chick weights investigated over three panels
 ui <- navbarPage(
   title = "Random Workouts",
   collapsible = FALSE,
   theme = bslib::bs_theme(),
-  tabPanel(title = "Exercises"),
-  tabPanel(title = "Randomizer"),
-  tabPanel(title = "Activity Log")
-)
-
+  tabsetPanel(
+    id="tabs",
+    tabPanel(title = "Exercises",DT::dataTableOutput("exercise_table")),
+    tabPanel(title = "Randomizer",
+             actionButton("rw","Randomize"),
+             actionButton("dw","Do this workout"),
+             DT::dataTableOutput("random_workout")),
+    tabPanel(title = "Activity Log",
+             DT::DTOutput("chosen_workout")
+             )
+))
 # Define server logic
-server <- function(input, output) {
-  exercises = googlesheets4::read_sheet("https://docs.google.com/spreadsheets/d/1_2tUyBnXQ3zsnnSoyy9gzK-hOpmnWqgd9fsM9v6sUW8/edit#gid=0",sheet = "Exercise DB")
-  render
+server <- function(input, output, session) {
+  # Display all possible exercises
+  exercises = googlesheets4::read_sheet(
+    "https://docs.google.com/spreadsheets/d/1_2tUyBnXQ3zsnnSoyy9gzK-hOpmnWqgd9fsM9v6sUW8/edit#gid=0",
+    sheet = "Exercise DB"
+  )
+output$exercise_table = DT::renderDataTable(exercises)
+
+# Generates a random workout the user can review to pick a workout
+observeEvent(input$rw,
+             {
+               random_workout = exercises %>% group_by(`Body part`) %>% slice_sample(n=1)
+               output$random_workout = DT::renderDataTable(random_workout)
+             }
+)
+# Copies the workout to activity log and gives input columns for resistance, sets and reps
+observeEvent(input$dw,
+             {
+              output$chosen_workout = DT::renderDT(random_workout,list(target='column',disable = list(columns=1)))
+              updateTabsetPanel(session = session, "tabs",selected = "Activity Log")
+             }
+)
 }
 
 shinyApp(ui, server)
