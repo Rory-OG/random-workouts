406dbd959433f632e5c68f6caceafd7cb44a0364
Rory O'Gallagher <rory.j.ogallagher@gmail.com>
2022-11-11 17:22:02 -0500

    reached MVP with end the ability to save to my google sheet

--- new_app/app.R
+++ new_app/app.R
@@ -1,50 +1,65 @@
 library(gridlayout)
-library(import)
 library(shiny)
 library(ggplot2)
 library(googlesheets4)
+library(googledrive)
 library(dplyr)
+library(DataEditR)
 
-# Chick weights investigated over three panels
+# Randomized workouts generated from exercise DB and recording in an activity log that appends to google sheets
 ui <- navbarPage(
   title = "Random Workouts",
   collapsible = FALSE,
   theme = bslib::bs_theme(),
-  tabsetPanel(
-    id="tabs",
-    tabPanel(title = "Exercises",DT::dataTableOutput("exercise_table")),
-    tabPanel(title = "Randomizer",
-             actionButton("rw","Randomize"),
-             actionButton("dw","Do this workout"),
-             DT::dataTableOutput("random_workout")),
-    tabPanel(title = "Activity Log",
-             DT::dataTableOutput("chosen_workout")
-             )
-))
+  id = "tabs",
+  tabPanel(
+    title = "Randomizer",
+    actionButton("rw", "Randomize"),
+    actionButton("dw", "Do this workout"),
+    DT::dataTableOutput("random_workout")),
+  tabPanel(title = "Activity Log",
+           dataEditUI("edit-1"),
+           actionButton("record","Save to Activity Log")),
+  tabPanel(title = "Exercises", DT::dataTableOutput("exercise_table"))
+)
 # Define server logic
 server <- function(input, output, session) {
   # Display all possible exercises
-  exercises = googlesheets4::read_sheet(
-    "https://docs.google.com/spreadsheets/d/1_2tUyBnXQ3zsnnSoyy9gzK-hOpmnWqgd9fsM9v6sUW8/edit#gid=0",
-    sheet = "Exercise DB"
-  )
+  # gs4_auth(cache = ".secrets", email = TRUE)
+  url <- "https://docs.google.com/spreadsheets/d/1_2tUyBnXQ3zsnnSoyy9gzK-hOpmnWqgd9fsM9v6sUW8/edit#gid=0"
+  exercises = googlesheets4::read_sheet(url,sheet = "Exercise DB")
   output$exercise_table = DT::renderDT(exercises)
   
   # Generates a random workout the user can review to pick a workout
   observe({
     random_workout = exercises %>% group_by(`Body part`) %>% slice_sample(n = 1) %>% ungroup()
-    rand_work <<- random_workout %>% select(-`Body part`) %>% mutate(sets = "",reps="")
+    rand_work <<- random_workout %>%
+      select(`Body part`, Exercise) %>%
+      mutate(Date = Sys.Date(),
+             resistance = "",
+             sets = "",
+             reps = "") %>%
+      relocate(Date) %>% 
+      reactiveValues(data = .)
     output$random_workout = DT::renderDT(random_workout)
   }) %>%
     bindEvent(input$rw) 
-    
   
-# Copies the workout to activity log and gives input columns for resistance, sets and reps
+  data_edit = dataEditServer("edit-1", data = rand_work$data)
+  # Copies the workout to activity log and gives input columns for resistance, sets and reps
   observe({
-    output$chosen_workout = DT::renderDT(rand_work,editable = list(target = 'column',disable = list(columns = 1)))
-    updateTabsetPanel(session = session,"tabs", selected = "Activity Log")
-  }) %>%
+    updateTabsetPanel(session = session, "tabs", selected = "Activity Log")
+    rand_work$data = data_edit()
+    }) %>%
     bindEvent(input$dw)
+  # handles output and save the data to an object 
+  dataOutputServer("output-1",rand_work$data)
+  
+  # writes the data to the google sheet
+  observe({
+    sheet_append(ss = url,data = rand_work$data,sheet = "Activity Log")
+  }) %>% 
+    bindEvent(input$record)
   }
-
+# writes the data to the 
 shinyApp(ui, server)
